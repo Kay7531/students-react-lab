d2a4f649c1decd12c7e537e6b8de6a5efd4918e6
User Name <kauth333@gmail.com>
2023-01-26 00:09:13 -0500

    create studentList component and add  sturdent prop to App parent component

---
+++ src/components/Score.jsx
@@ -0,0 +1,11 @@
+import { studentData } from "../data/data"
+function Score(props) {
+    return (
+        <>
+     
+        
+        </>
+    )
+}
+
+export default Score
---
+++ src/components/StudentList.jsx
@@ -0,0 +1,13 @@
+import { studentData } from "../data/data"
+import Score from "./Score"
+function StudentList(props) {
+    return(
+        <>
+        <h1> This is StudentList component.</h1>
+     
+
+        </>
+    )
+    }
+    
+    export default StudentList
